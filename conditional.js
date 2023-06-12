9a8a3f888d00d2ef43b7b1f37faaff285fcfcc36
Ansu <aryalansu2064@gmail.com>
2023-06-12 14:29:56 +0545

    operator

---
+++ conditional.js
@@ -0,0 +1,20 @@
+//conditional in javascript
+/*
+Type 
+1. If
+2. If else
+3. If,else if,else
+4.switch
+
+1. if 
+syntax;
+If(condition){
+    //body of truth statement
+}
+
+
+*/
+var a = 12;
+if (a > 10) {
+    console.log(a + " is greater then 10")
+}
